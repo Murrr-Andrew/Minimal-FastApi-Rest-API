26e245d4d21febc0789624cfe3f55a7eba0eb30a
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-23 21:08:15 +0200

    FastApi: Init

---
+++ help.py
@@ -0,0 +1,16 @@
+# === GIT =======================================
+# git config user.email "myemail@gmail.com"
+# git config user.email
+
+# === VENV ======================================
+# python3 -m venv venv -> installing virtual environment
+# source venv/bin/activate -> activating virtual environment
+# deactivate -> deactivate virtual environment
+
+# === REQUIREMENTS ==============================
+# pip3 freeze > requirements.txt
+
+# === FastApi ===================================
+# pip3 install fastapi
+# pip3 install unicorn
+# pip3 install pydantic
