54a45dd9336db4c8440491ad03b349fdd03c588c
Andrew Murrow <murrr.andrew@gmail.com>
2023-01-23 22:02:01 +0200

    FastApi: Get with params

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from typing import Optional
 import os
@@ -23,7 +23,15 @@ with open(file, 'r') as f:
     people = json.load(f)
 
 
-@app.get('/person/{p_id}')
+@app.get('/person/{p_id}', status_code=200)
 def get_person(p_id: int):
     person = [p for p in people if p['id'] == p_id]
     return person[0] if len(person) else {}
+
+
+@app.get('/search/', status_code=200)
+def search_person(age: Optional[int] = Query(None, title='Age', description='The age to filter for'),
+                  name: Optional[str] = Query(None, title='Name', description='The name to filter for')):
+    person = [p for p in people if (age and age == p['age']) or (name and name.lower() in p['name'].lower())]
+
+    return people if name is None and age is None else person
